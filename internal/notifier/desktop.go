b33dd2ebdd87b6fba366a055b2d21b7c4ef47382
Trung <trungnguyenquochn2202@gmail.com>
2026-02-16 17:37:32 -0500

    add watch and notifications

---
+++ internal/notifier/desktop.go
@@ -0,0 +1,40 @@
+package notifier
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+
+	"github.com/Trungsherlock/jobgocli/internal/database"
+)
+
+type DesktopNotifier struct {}
+
+func NewDesktopNotifier() *DesktopNotifier {
+	return &DesktopNotifier{}
+}
+
+func (d *DesktopNotifier) Notify(job database.Job, companyName string, score float64) error {
+	title := "JobGo: New Match"
+	body := fmt.Sprintf("[%.0f] %s @ %s", score, job.Title, companyName)
+
+	switch runtime.GOOS {
+	case "windows":
+		script := fmt.Sprintf(
+			`[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null; `+
+			`$template = [Windows.UI>Notifications.ToastNotificationManager]::GetTemplateContent(0); `+
+			`$text = $template.GetElementsByTagName('text'); `+
+			`$text.Item(0).AppendChild($template.CreateTextNode('%s')) > $null; `+
+			`$text.Item(1).AppendChild($template.CreateTextNode('%s')) > $null; `+
+			`$toast = [Windows.UI.Notifications.ToastNotification]::new($template); `+
+			`[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('JobGo').Show($toast)`,
+			title, body,
+		)
+		return exec.Command("powershell", "-Command", script).Start()
+	case "darwin":
+		script := fmt.Sprintf(`display notification "%s" with title "%s"`, body, title)
+		return exec.Command("osascript", "-e", script).Start()
+	default:
+		return exec.Command("notify-send", title, body).Start()
+	}
+}
---
+++ internal/notifier/terminal.go
@@ -0,0 +1,23 @@
+package notifier
+
+import (
+	"fmt"
+
+	"github.com/Trungsherlock/jobgocli/internal/database"
+)
+
+type TerminalNotifier struct{}
+
+func NewTerminalNotifier() *TerminalNotifier {
+	return &TerminalNotifier{}
+}
+
+func (t *TerminalNotifier) Notify(job database.Job, companyName string, score float64) error {
+	location := ""
+	if job.Location != nil {
+		location = *job.Location
+	}
+	fmt.Printf("	NEW MATCH [%.0f] %s @ %s (%s)\n", score, job.Title, companyName, location)
+	fmt.Printf("	-> %s\n", job.URL)
+	return nil
+}
